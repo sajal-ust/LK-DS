74568666af0c493e8aeb58dd69192d2c82069eb5
sameerpatil04 <sameer.patil@ust.com>
2025-04-14 19:08:46 +0530

    gpt_model.py

--- LKEA-AI-Project/Sentiment_Analysis_NLP_Model/gpt_model.py
+++ LKEA-AI-Project/Sentiment_Analysis_NLP_Model/gpt_model.py
@@ -7,7 +7,7 @@ from sklearn.metrics import classification_report
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
+print("API Key:", openai.api_key)
 if not openai.api_key:
     raise ValueError("API key not found.")
 def get_gpt_sentiment(text):
